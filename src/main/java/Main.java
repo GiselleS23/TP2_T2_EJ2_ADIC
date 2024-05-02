964b4dc7acdef314043ba157ae97875a6ccdd314
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-02 21:18:39 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,23 @@
+
+/* 2.- Un comerciante tributa mensualmente un impuesto provincial a los ingresos brutos, que es el 25% del total de ventas. Conociendo dicho total de ventas y el total de gastos del mes, se desea calcular la ganancia mensual del comerciante */
+
+
+import java.util.Scanner;
+
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    double ventasMes, gastosMes, gananciaMes, ingBrutosMes;
+    System.out.println("Ingrese el total de ventas del mes: ");
+    ventasMes = scanner.nextDouble();
+    System.out.println("Ingrese el total de gastos del mes: ");
+    gastosMes = scanner.nextDouble();
+    ingBrutosMes = ventasMes*0.25;
+    System.out.println("El ingreso bruto del mes es: " + ingBrutosMes);
+    scanner.close();
+
+  }
+  
+ 
+}
