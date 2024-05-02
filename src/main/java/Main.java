c88b6286c062f9c05ea8abcdc29e8127b7ef1ce1
GiselleS23 <128999381+GiselleS23@users.noreply.github.com>
2024-05-02 23:32:01 +0000

    validacion para que ventasMes mayor a cero con while

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 
 /* 2.- Un comerciante tributa mensualmente un impuesto provincial a los ingresos brutos, que es el 25% del total de ventas. Conociendo dicho total de ventas y el total de gastos del mes, se desea calcular la ganancia mensual del comerciante */
 
-
 import java.util.Scanner;
-
 
 public class Main {
   public static void main(String[] args) {
@@ -11,17 +9,32 @@ public class Main {
     double ventasMes, gastosMes, gananciaMes, ingBrutosMes;
     double ingB = 0.25;
     
-    System.out.println("Ingrese el total de ventas del mes: ");
-    ventasMes = scanner.nextDouble();
-    
-    System.out.println("Ingrese el total de gastos del mes: ");
-    gastosMes = scanner.nextDouble();
-    //calculo de los ingresos brutos
-    ingBrutosMes = ventasMes*ingB;
-    System.out.println("El ingreso bruto del mes es: " + ingBrutosMes);
-    scanner.close();
 
+    System.out.println("Ingrese el total de ventas del mes: ");
+
+    ventasMes = scanner.nextDouble();
+
+    while (ventasMes <= 0) {
+      System.out.println("Ingrese un valor valido para las ventas del mes: ");
+      ventasMes = scanner.nextDouble();
+    }
+
+    if (ventasMes > 0) {
+      System.out.println("Ingrese el total de gastos del mes: ");
+      gastosMes = scanner.nextDouble();
+      // calculo de los ingresos brutos
+      ingBrutosMes = ventasMes * ingB;
+      System.out.println("El ingreso bruto del mes es: " + ingBrutosMes);
+
+      /*
+       * Calculamos la ganancia mensual del comerciante restando los gastos
+       * y el impuesto de las ventas:
+       * ej Ganancia=Total de ventas−Total de gastos−Impuesto=5000−3000−1250=750
+       */
+      gananciaMes = ventasMes - gastosMes - ingBrutosMes;
+      System.out.println("La ganancia mensual del comerciante es: " + gananciaMes);
+    }
+    scanner.close();
   }
-  
- 
+
 }
